c0d29129bc320d2fe205d6e2d0f60161658dd993
Nikita Bulgakov <nbulgakov89@gmail.com>
2025-03-19 00:27:49 +0300

    refactor: update namespace creation to use kubectl apply with YAML style: enhance HTML with button and footer elements cleanup: remove unused Docker build and push commands from create.sh
    Signed-off-by: Nikita Bulgakov <nbulgakov89@gmail.com>

--- create.sh
+++ create.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-export REGISTRY_ID=$(yc container registry get --name diploma-container-registry --folder-name netology-1 --format json | jq .id -r)
 export CLUSTER_ID=$(yc managed-kubernetes cluster get --name diploma-cluster --folder-name netology-1 --format json | jq .id -r)
 # Получение кредов для кластера
 yc managed-kubernetes cluster get-credentials $CLUSTER_ID --external --force
-# Создание образа и пуш в реестр
-docker build container/ -t cr.yandex/$REGISTRY_ID/my-test-app:latest
-docker push cr.yandex/$REGISTRY_ID/my-test-app:latest
 # Установка репозиториев helm
 helm repo add external-secrets https://charts.external-secrets.io
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -20,7 +16,12 @@ kubectl wait \
 kubectl apply -f k8s/kube-prometheus/manifests/
 # Установка external-secrets, создание секрета и установка ingress-nginx
 helm install external-secrets external-secrets/external-secrets --namespace external-secrets --create-namespace
-kubectl create namespace ns
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+    name: ns
+EOF
 kubectl --namespace ns create secret generic yc-auth --from-file=authorized-key=.authorized_key.json
 helm install ingress-nginx ingress-nginx/ingress-nginx --set controller.extraArgs.default-ssl-certificate="ns/k8s-secret"
 sleep 15
